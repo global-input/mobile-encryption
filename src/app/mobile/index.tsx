a1603a9b1ae48fd0fa35a526746ad255500a2156
Dilshat Hewzulla <hewzulla@gmail.com>
2020-11-25 10:18:45 +0000

    cleaning codes

--- src/app/mobile/index.tsx
+++ src/app/mobile/index.tsx
@@ -1,6 +1,26 @@
-import useMobile from './useMobile';
-import userWithDomainAsFormId from './userWithDomainAsFormId';
-import MobileConnect from './MobileConnect';
-export * from 'global-input-react';////global-input-react////
 
-export { useMobile, userWithDomainAsFormId, MobileConnect };
+import { useCallback } from 'react';
+
+import * as globalInput from 'global-input-react';////global-input-react////
+////main////
+import * as storage from '../storage';
+export const useMobile = (initData: globalInput.InitData | (() => globalInput.InitData), connect: boolean = true) => {
+    const connectionSettings = storage.loadConnectionSettings();
+    const options: globalInput.ConnectOptions = {
+        url: connectionSettings.url,////use your own server"
+        apikey: connectionSettings.apikey,
+        securityGroup: connectionSettings.securityGroup
+    };
+    const mobile = globalInput.useGlobalInputApp({
+        initData, options, codeAES: connectionSettings.codeKey
+    }, connect);
+
+    const setOnFieldChange = useCallback((onFieldChange: (field: globalInput.FormField) => void) => {
+        mobile.setOnchange(({ field }) => {
+            onFieldChange(field);
+        })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [mobile.setOnchange]);
+    ////dev-test codeData
+    return { ...mobile, setOnFieldChange };
+};
